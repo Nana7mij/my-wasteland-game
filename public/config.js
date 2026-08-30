03d455aa2e501d82d13696f1fa627d6bdc5fc189
Nana7mij <1179291540@qq.com>
2026-08-30 11:08:23 +0800

    feat: initial commit for github pages

---
+++ public/config.js
@@ -0,0 +1,12 @@
+// 装备与道具数据库配置
+const ITEMS_DATABASE = [
+    { name: "生锈管钳", type: "weapon", rarity: "common", power: 5, price: 10, affix: "普通打击" },
+    { name: "战术匕首", type: "weapon", rarity: "uncommon", power: 15, price: 30, affix: "+5% 攻速" },
+    { name: "废土打铁步枪", type: "weapon", rarity: "rare", power: 45, price: 100, affix: "+10% 暴击" },
+    { name: "高能等离子枪", type: "weapon", rarity: "epic", power: 120, price: 350, affix: "附带灼烧效果" },
+    { name: "【遗物】高斯狙击炮", type: "weapon", rarity: "legendary", power: 350, price: 1000, affix: "穿透护甲 +30%" },
+    { name: "【神级】灭世核光发射器", type: "weapon", rarity: "mythic", power: 999, price: 5000, affix: "全屏清场" },
+    { name: "破损皮夹克", type: "armor", rarity: "common", power: 3, price: 8, affix: "基础御寒" },
+    { name: "凯夫拉防弹衣", type: "armor", rarity: "rare", power: 35, price: 90, affix: "伤害减免 5%" },
+    { name: "动力装甲胸甲", type: "armor", rarity: "legendary", power: 280, price: 850, affix: "自动修复生命值" }
+];
